b2e6d7fee4204d5d8d6c9edabb5bca30f4018f5b
Anthony Rojo <aarojo1@up.edu.ph>
2026-04-04 19:30:11 +0800

    Update page.tsx

--- app/(dashboard)/tasks/page.tsx
+++ app/(dashboard)/tasks/page.tsx
@@ -34,6 +34,7 @@ export default function TasksPage() {
     addTaskComment,
     updateTaskComment,
     deleteTaskComment,
+    currentTeam,
   } = useApp();
 
   const [searchQuery, setSearchQuery] = useState("");
